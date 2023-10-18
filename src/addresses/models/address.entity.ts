171dcde8b831dfe4fb5f278b2163dcbbfd9c9be8
va_kam <tulevskiv@gmail.com>
2023-10-19 00:09:45 +0300

    address entity and repo

---
+++ src/addresses/models/address.entity.ts
@@ -0,0 +1,31 @@
+import { Order } from 'src/orders/models/order.entity';
+import { User } from 'src/users/models/user.entity';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, CreateDateColumn, UpdateDateColumn, OneToMany } from 'typeorm';
+
+
+@Entity()
+export class Address {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  city: string;
+
+  @Column()
+  street: string;
+
+  @Column({ nullable: true })
+  additionalNotes: string;
+
+  @ManyToOne(() => User, (user) => user.addresses)
+  user: User;
+
+  @OneToMany(() => Order, (order) => order.address)
+  orders: Order[];
+
+  @CreateDateColumn({ type: 'timestamp' })
+  dateCreated: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  dateModified: Date;
+}
