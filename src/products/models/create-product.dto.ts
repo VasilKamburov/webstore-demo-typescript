bc85079c2f4290fd20a5932434825f199386156e
va_kam <tulevskiv@gmail.com>
2023-10-19 00:10:56 +0300

    Added validation to product creation dto's fields

--- src/products/models/create-product.dto.ts
+++ src/products/models/create-product.dto.ts
@@ -1,11 +1,19 @@
+import { IsInt, IsNotEmpty, IsNumber, IsNumberString, Min } from "class-validator";
+
 export class CreateProductDto {
+    @IsNotEmpty()
     title: string;
 
+    @IsNotEmpty()
     description: string;
 
+    @IsNotEmpty()
     size: string;
 
+    @IsNotEmpty()
     colour: string;
 
+    @IsInt()
+    @Min(0)
     quantityAvailable: number;
 }
